80f04b06534c4dba0d9832d4175cdd269e20825e
Janam <Janam@Lmt10Hunk>
2023-11-03 16:17:15 +0100

    CredentialException created

---
+++ src/Exceptions/CredentialException.java
@@ -0,0 +1,26 @@
+package Exceptions;
+
+/**
+ *
+ * @author Janam
+ */
+public class CredentialException extends Exception {
+
+    /**
+     * Creates a new instance of <code>CredentialException</code> without
+     * detail message.
+     */
+    public CredentialException() {
+    }
+
+    /**
+     * Constructs an instance of <code>CredentialException</code> with
+     * the specified detail message.
+     *
+     * @param message the detail message.
+     */
+    public CredentialException(String message) {
+        super(message);
+    }
+
+}
